7d45d4fae7d7e910ba7a1c53330ea120017295a6
Juarez <ajuarez@iastate.edu>
2019-04-04 13:31:05 -0500

    lab 8done

--- Lab8/main.c
+++ Lab8/main.c
@@ -22,29 +22,37 @@ int main(void) {
 
         if (button_num == 1) {
             servo_position = servo_move(1);
+            button_num=0;
         }
         else if (button_num == 2) {
             servo_position = servo_move(2.5);
+            button_num=0;
         }
         else if (button_num == 3) {
             servo_position = servo_move(5);
+            button_num=0;
         }
         else if (button_num == 4) {
             if (movement_direction == 0) {
-                movement_direction == 1;
+                movement_direction = 1;
             }
             else {
-                movement_direction == 0;
+                movement_direction = 0;
             }
+            button_num=0;
         }
         else if (button_num == 5) {
             servo_position = servo_move(0);
+            button_num=0;
         }
         else if (button_num == 6) {
             servo_position = servo_move(180);
+            button_num=0;
+
         }
 
-        lcd_printf("Servo position: %d\nDirection: %d", servo_position, movement_direction); //Todo direction
+
+        lcd_printf("Servo position: %d\nDirection: %d", servo_position, movement_direction);
 
     }
 }
--- Lab8/servo.c
+++ Lab8/servo.c
@@ -10,8 +10,10 @@
 #include "servo.h"
 #include <math.h>
 
+#define match0_p;
+#define match0;
 volatile int servo_position = 90;
-volatile int movement_direction = 0; //1 == counterclockwise, c == clockwise
+volatile int movement_direction = 1; //1 == counterclockwise, c == clockwise
 
 /**
  * Initilize servo motor with timer in PWM mode
@@ -23,33 +25,37 @@ volatile int movement_direction = 0; //1 == counterclockwise, c == clockwise
  */
 void servo_init(void) {
     //Setup GPIO
-        SYSCTL_RCGCGPIO_R |= 0b10; //Enable clock on Port B
-        SYSCTL_RCGCTIMER_R |= 0b10; //Enable timer 1 clock
+    SYSCTL_RCGCGPIO_R |= 0b10; //Enable clock on Port B
+    SYSCTL_RCGCTIMER_R |= 0b10; //Enable timer 1 clock
 
-        GPIO_PORTB_DIR_R |= 0x20; //Set pin 5 output
-        GPIO_PORTB_DEN_R |= 0x20; //set pin 5 as digital mode
+    GPIO_PORTB_DIR_R |= 0x20; //Set pin 5 output
+    GPIO_PORTB_DEN_R |= 0x20; //set pin 5 as digital mode
 
-        GPIO_PORTB_AFSEL_R |= 0b00100000; //Use alternate function on port 5
-        GPIO_PORTB_PCTL_R |= 0x00700000; //Use function T1CCP1
+    GPIO_PORTB_AFSEL_R |= 0b00100000; //Use alternate function on port 5
+    GPIO_PORTB_PCTL_R |= 0x00700000; //Use function T1CCP1
 
 
     //Setup Timer 1B
-        TIMER1_CTL_R &= 0xFFFFFEFF; //Disable timer while we set it up
-        //DISABLE TB INSTEA DOF TA
+    TIMER1_CTL_R &= 0xFFFFFEFF; //Disable timer while we set it up
+    //DISABLE TB INSTEA DOF TA
 
-        TIMER1_CFG_R = 0x4; //Set to 16-bit mode
+    TIMER1_CFG_R = 0x4; //Set to 16-bit mode
 
-        TIMER1_TBMR_R |= 0b1010; //PWM, edge count, periodic timer //0b00000001010
-        TIMER1_TBMR_R &= 0xFFFFFFFA;
+    TIMER1_TBMR_R |= 0b1010; //PWM, edge count, periodic timer //0b00000001010
+    TIMER1_TBMR_R &= 0xFFFFFFFA;
 
-        TIMER1_TBPR_R = 0x0004; //Use prescaler extension to 24 bits
-        TIMER1_TBILR_R = 0xE200; //Load max 24-bit value
+    //  TIMER1_TBPR_R = 0x0005; //Use prescaler extension to 24 bits
+    //TIMER1_TBILR_R = 0x3FC0; //Load max 24-bit value
 
-        TIMER1_TBPMR_R = 0x0000; //use prescaler extension to 24 bits
-        TIMER1_TBMATCHR_R = 0x5DC0; //load match value
-                                                                                                //todo Initilize to 90 degrees
-        TIMER1_CTL_R &= 0x0D00;
-        TIMER1_CTL_R |= 0x0D00; //enable //0b0000110100000000 //both edges, enable timer
+    TIMER1_TBPR_R = 0x0004; //Use prescaler extension to 24 bits
+    TIMER1_TBILR_R = 0xE200; //Load max 24-bit value 320000
+
+
+    TIMER1_TBPMR_R = 0x0004; //use prescaler extension to 24 bits
+    TIMER1_TBMATCHR_R = 0x8440; //load match value
+    //todo Initilize to 90 degrees
+    TIMER1_CTL_R &= 0x0D00;
+    TIMER1_CTL_R |= 0x0D00; //enable //0b0000110100000000 //both edges, enable timer
 
 }
 
@@ -63,22 +69,54 @@ void servo_init(void) {
  */
 int servo_move(float degrees) {
 
-    if (degrees == 180) { //abs(degrees - 180) < FLT_EPSILON) {
-        //move to 180 degrees
+    if (movement_direction == 1) {
+        servo_position += degrees;
+    }
+    else {
+        servo_position -= degrees;
+    }
+    if(servo_position >=180 && movement_direction ==1){
+           if(degrees ==0){
+               TIMER1_TBPMR_R = 0x0004;
+               TIMER1_TBMATCHR_R =  0xC2C0;
+                      return 0;
+           }
+           TIMER1_TBPMR_R = 0x0004;
+           TIMER1_TBMATCHR_R = 0x5240;
+           return 180;
+       }
+       if(servo_position <=0 && movement_direction ==0){
+           if(degrees ==180){
+               TIMER1_TBPMR_R = 0x0004;
+               TIMER1_TBMATCHR_R = 0x524;
+                      return 180;
+           }
+           TIMER1_TBPMR_R = 0x0004;
+           TIMER1_TBMATCHR_R =  0xC2C0;
+           return 0;
+       }
+
+    int match = (-160)*servo_position + 312000;
+    int match_r = match & 0xFFFF;
+    int match_prescale = match >> 16;
+
+    if (degrees == 180) {
+        //move to 180 degrees'
+
+        TIMER1_TBPMR_R = 0x0004;
+        TIMER1_TBMATCHR_R = 0x5240; // (.002*16000000) *2
         return 180;
     }
-    else if (degrees == 0) {//abs(degrees - 0) < FLT_EPSILON) {
-        //move to 0 degrees
+    else if (degrees == 0) {
+        TIMER1_TBPMR_R = 0x0004;
+        TIMER1_TBMATCHR_R =  0xC2C0; // (.001*16000000) /2
         return 0;
     }
     else {
-        //move degrees degrees
-        if (movement_direction == 0) {
-            return servo_position + degrees;
-        }
-        else {
-            return servo_position - degrees;
-        }
+        //move degrees
+        TIMER1_TBPMR_R = match_prescale;
+        TIMER1_TBMATCHR_R = match_r;
+        return servo_position;
     }
 
 
